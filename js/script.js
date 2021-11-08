2dc936f28b4d3882e94878d86e69bd5e503a03c4
MwitaBrian <brianmwita13@gmail.com>
2021-11-08 18:46:37 +0300

    Create instances of Pizza object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Constructor
 function Pizza(size, toppings, crust, total, orderNo){
   this.size = size;
   this.toppings = toppings;
@@ -6,3 +7,8 @@ function Pizza(size, toppings, crust, total, orderNo){
   this.order = orderNo;
 
 }
+
+//Create instances of the Pizza object
+let firstPizza = new Pizza("large", "pepperoni", "crispy", 1200, 1);
+let secondPizza = new Pizza("medium", "cheese", "gluten-free", 800, 2);
+let thirdPizza = new Pizza("small", "onions", "soft", 500, 3);
